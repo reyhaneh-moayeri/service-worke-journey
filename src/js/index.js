e382a88a0e98843475b9bc1fccf1ad8e52da8be8
Reyhaneh Moayerirad <reyhanehrm333@gmail.com>
2022-11-16 04:59:52 -0800

    send message based on the user connectivity and loggedIn state

--- src/js/index.js
+++ src/js/index.js
@@ -22,11 +22,13 @@
         window.addEventListener("online", function online() {
             isOnline = true
             offlineIcon.textContent = 'online'
+                sendStatusUpdate()
         })
 
         window.addEventListener("offline", function online() {
             isOnline = false
             offlineIcon.textContent = 'offline'
+                sendStatusUpdate()
         })
     }
 
@@ -40,14 +42,46 @@
         // when there is a second one installed, it goes to waiting phase. waiting for the first one to finish
 
         sw = swRegistration.installing || swRegistration.waiting || swRegistration.active
+        sendStatusUpdate(sw)
 
         // when a new service worker come in
         
         navigator.serviceWorker.addEventListener("controllerchange", function onController() {
             sw = navigator.serviceWorker.controller
+            sendStatusUpdate(sw)
             
         })
+
+        navigator.serviceWorker.addEventListener("message", onSWMessage)
             
+    }
+
+    function onSWMessage(evt) {
+        let { data } = evt
+        console.log(data)
+        if (data.requestStatusUpdate) {
+            console.log("received status update from serviceworker")
+            // who do we wanna send status update? there will be multiple cases that we wanna communicate with
+            // like the service worker will communicate with multiple pages so it end up using something called
+            // message channel to make this connections. so it ends up creating some unique set of ports that
+            //those communication may happen
+            sendStatusUpdate(evt.ports && evt.ports[0])     
+        }
+        
+    }
+
+    function sendStatusUpdate(target) {
+        sendswMessage({ statusUpdate: { isOnline, isLoggedIn } }, target );
+    }
+
+    async function sendswMessage(msg , target) {
+        if (target) {
+            target.postMessage(msg)
+        } else if (sw) {
+            sw.postMessage(msg)
+        } else {
+            navigator.serviceWorker.controller.postMessage(msg)
+        }
     }
 
 })()
--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
 "use strict";
 
-const version = 1
+const version = 2
+
+let isOnline = true
+let isLoggedIn = false
+
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivated);
+self.addEventListener("message", onMessage);
 
 main().catch(console.error)
 
 async function main() {
     console.log(`Service worker (${version}) is starting ...`)
+    await sendMessage({requestStatusUpdate : true})
+}
+
+
+async function sendMessage(msg) {
+    var allClients = await clients.matchAll({ includeUncontrolled: true })
+    return Promise.all(
+        allClients.map(function clientMsg(client) {
+            let channel = new MessageChannel()
+            channel.port1.onmessage = onMessage
+            return client.postMessage(msg , [channel.port2])
+        })
+    )
+
+}
+
+function onMessage({ data }) {
+	if ("statusUpdate" in data) {
+		({ isOnline, isLoggedIn } = data.statusUpdate);
+		console.log(`Service Worker (v${version}) status update... isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`);
+	}
 }
 
 async function onInstall(evt) {
@@ -24,3 +50,5 @@ async function handleActivation() {
     console.log(`Service worker (${version}) is activating ...`)
 
 }
+
+//service workers don't have access to the local storage. they do have index db and can store stuff in index db
